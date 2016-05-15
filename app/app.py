6b7f1cbbc2935dba00a9732e0f5672ee87d0a98d
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-05-15 18:14:28 +0200

    added code for getting absolute paths as required by pythonanywhere

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from PostRenderer import PostRenderer
+import os
 
 app = Flask(__name__)
 post_renderer = PostRenderer()
+
+current_file_directory = os.path.dirname(__file__)
+
+def static_file_path(relative_path):
+	return os.path.join(current_file_directory, relative_path)
 
 @app.route('/')
 @app.route('/index')
@@ -12,17 +18,17 @@ def index():
 
 @app.route('/lf2/blog')
 def lf2_blog():
-	complete_html_content = post_renderer.render_posts_from('app/static/markdown/lf2/blog/')
+	complete_html_content = post_renderer.render_posts_from(static_file_path('static/markdown/lf2/blog/'))
 	return render_template('lf2_blog.j2', title='LF2 Blog', content=complete_html_content)
 
 @app.route('/lf2/intro')
 def lf2_intro():
-	complete_html_content = post_renderer.render_posts_from('app/static/markdown/lf2/intro/')
+	complete_html_content = post_renderer.render_posts_from(static_file_path('static/markdown/lf2/intro/'))
 	return render_template('lf2_blog.j2', title='Little Fighter 2', content=complete_html_content)
 
 @app.route('/chinese/blog')
 def mandarin_blog():
-	complete_html_content = post_renderer.render_posts_from('app/static/markdown/chinese/blog/')
+	complete_html_content = post_renderer.render_posts_from(static_file_path('static/markdown/chinese/blog/'))
 	return render_template('chinese_blog.j2', title='Chinese Blog', content=complete_html_content)
 
 @app.route('/coding/blog')
