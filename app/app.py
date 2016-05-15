9983dc0b3277a9079a0883de6ad93274d778c23f
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-05-15 17:44:20 +0200

    dev prefix removed again

--- app/app.py
+++ app/app.py
@@ -12,17 +12,17 @@ def index():
 
 @app.route('/lf2/blog')
 def lf2_blog():
-	complete_html_content = post_renderer.render_posts_from(dev_prefix + '/static/markdown/lf2/blog/')
+	complete_html_content = post_renderer.render_posts_from('app/static/markdown/lf2/blog/')
 	return render_template('lf2_blog.j2', title='LF2 Blog', content=complete_html_content)
 
 @app.route('/lf2/intro')
 def lf2_intro():
-	complete_html_content = post_renderer.render_posts_from(dev_prefix + '/static/markdown/lf2/intro/')
+	complete_html_content = post_renderer.render_posts_from('app/static/markdown/lf2/intro/')
 	return render_template('lf2_blog.j2', title='Little Fighter 2', content=complete_html_content)
 
 @app.route('/chinese/blog')
 def mandarin_blog():
-	complete_html_content = post_renderer.render_posts_from(dev_prefix + '/static/markdown/chinese/blog/')
+	complete_html_content = post_renderer.render_posts_from('app/static/markdown/chinese/blog/')
 	return render_template('chinese_blog.j2', title='Chinese Blog', content=complete_html_content)
 
 @app.route('/coding/blog')
@@ -33,7 +33,5 @@ def coding_blog():
 def coding_resources():
 	return render_template('coding_resources.j2', title='Coding Resources', content='Coding Resources (no resources here yet :)')
 
-dev_prefix = ''
 if __name__ == '__main__':
 	app.run(debug=True)
-	dev_prefix = 'app'
